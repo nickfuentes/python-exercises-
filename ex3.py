b57542785f4168bcf55bea997901ac50adb227bd
Nickolas Fuentes <>
2019-06-26 15:50:36 -0500

    Practiced printing out strings with operators and numbers. Mathing.

---
+++ ex3.py
@@ -0,0 +1,37 @@
+"""+ plus
+- minus
+/ slash
+* asterik
+% percent
+< less-than
+> greater-than
+<= less-than-equal
+>= greater-than-equal"""
+
+# Prints out the string "I will now count my chickens:"
+print "I will now count my chickens:"
+# Prints out the string "Hens" uses PEMDAS 30 divided by 6 than adds 25.
+print "Hens", 25 + 30 / 6
+# Prints out the string "Roosters" than 25 * 5 than % 4 than - 100
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints the string "Now I will count the eggs;"
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+# Prints out sting "What is 3 + 2?" than adds 3 + 2 and prints the int
+print"What is 3 + 2?", 3 + 2
+print "What is 5 - 7?",  5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
