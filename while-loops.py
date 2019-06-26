8e648b389ed34d74e89a15462a78c1f932194e19
Nickolas Fuentes <>
2019-06-26 15:59:39 -0500

    Created a while loop that prints hello until the while loop is stopped. Asks user to enter a number in a while loop until user types in q to stop the loop.

---
+++ while-loops.py
@@ -0,0 +1,16 @@
+counter = 1
+
+# While loop that prints out the string 9 times or < 10
+while counter < 10:
+    print("Hello World!")
+    # counter = counter + 1
+    counter += 1
+
+print("Outside of while loop!")
+
+user_input = ""
+
+# While loop will keep asking the user to enter a number until the user enters "q"
+# !=  mean not equal too
+while user_input != "q":
+    user_input = input("Enter a number: ")
