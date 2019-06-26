191b69927194489101bd5e749dbf409c04b9b1c2
Nickolas Fuentes <>
2019-06-26 15:53:37 -0500

    Created conditions with if, elif, and else statements

---
+++ conditions.py
@@ -0,0 +1,22 @@
+# if 1 < 2:
+# print("1 is less than 2")
+
+number = 3
+
+# if number > 2:
+#print("Number is greater than 2")
+
+"""if number < 2:
+    print(f"{number} is less than 2")
+elif number < 4:
+    print(f"{number} is less than 4")
+elif number > 10:
+    print("Number is greater than 10")
+else:
+    print("All conditions above are false")
+
+
+name = "Nick"
+
+if name == "Nick":
+    print(f"Hello {name}!")"""
